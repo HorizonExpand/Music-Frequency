083dc529718cac441e532b109b7e624b3f14e0c1
Hrumer <111180511+Hrumer@users.noreply.github.com>
2026-05-11 19:59:07 +0300

    Current music Toast

--- src/main/java/net/hrumer/music_frequency/mixins/MusicManagerMixin.java
+++ src/main/java/net/hrumer/music_frequency/mixins/MusicManagerMixin.java
@@ -1,8 +1,15 @@
 package net.hrumer.music_frequency.mixins;
 
+import com.mojang.logging.LogUtils;
 import net.hrumer.music_frequency.Config;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.components.toasts.SystemToast;
+import net.minecraft.client.resources.sounds.Sound;
+import net.minecraft.client.resources.sounds.SoundInstance;
 import net.minecraft.client.sounds.MusicManager;
-
+import net.minecraft.client.sounds.SoundManager;
+import net.minecraft.network.chat.Component;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,6 +21,11 @@ public class MusicManagerMixin {
 
     @Shadow
     private int nextSongDelay;
+    @Shadow
+    private SoundInstance currentMusic;
+
+    @Final
+    private Minecraft minecraft;
 
     @Inject(method = "tick", at = @At("HEAD"))
     private void onTick(CallbackInfo ci) {
@@ -21,5 +33,36 @@ public class MusicManagerMixin {
             float modifiedDelay = (float) (nextSongDelay / Config.musicFrequencyMultiplier);
             nextSongDelay = Math.max(1, (int) modifiedDelay);
         }
+    }
+
+    @Inject(method = "startPlaying", at = @At("TAIL"))
+    private void showToast(CallbackInfo ci) {
+        if (currentMusic == null || currentMusic.getSound() == SoundManager.EMPTY_SOUND) {
+            return;
+        }
+
+        String currentMusicName = getCurrentMusicTranslationKey();
+
+        if (currentMusicName != null) {
+            LogUtils.getLogger().info("Current musN: {}", currentMusicName);
+            Component title = Component.translatable("gui.now_playing")
+                    .withStyle(style -> style.withBold(true).withColor(0x55FF55));
+
+            Component subtitle = Component.translatable(currentMusicName.replace("/", "."))
+                    .withStyle(style -> style.withItalic(true));
+
+            SystemToast.add(minecraft.getToasts(), SystemToast.SystemToastIds.TUTORIAL_HINT, title, subtitle);
+        }
+    }
+
+    public String getCurrentMusicTranslationKey() {
+        if (currentMusic != null) {
+            Sound sound = currentMusic.getSound();
+            if (sound != null) {
+                return sound.getLocation().toShortLanguageKey();
+            }
+        }
+
+        return null;
     }
 }
